ccc8dc58433227dad37afedc7a45838e5d835738
Adzhan Farhan <adzhanfarhan@gmail.com>
2025-12-08 03:55:03 +0700

    feat: add fillable and casts properties to Reward model

--- app/Models/Reward.php
+++ app/Models/Reward.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reward extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'rating',
+        'description',
+        'review_date',
+    ];
+
+    protected $casts = [
+        'review_date' => 'datetime',
+    ];
 }
