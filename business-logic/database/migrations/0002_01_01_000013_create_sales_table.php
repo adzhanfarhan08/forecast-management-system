24ac5f9c36df103a027563c3e306175572623bea
Adzhan Farhan <adzhanfarhan@gmail.com>
2025-12-13 12:17:08 +0700

    feat: migration update

--- business-logic/database/migrations/0002_01_01_000013_create_sales_table.php
+++ business-logic/database/migrations/0002_01_01_000013_create_sales_table.php
@@ -13,15 +13,30 @@ return new class extends Migration
     {
         Schema::create('sales', function (Blueprint $table) {
             $table->id();
-            $table->integer('quantity');
-            $table->decimal('price', 10, 2);
-            $table->decimal('total', 10, 2)->nullable();
-            $table->date('sale_date');
+
+            // User yang melakukan transaksi
+            $table->foreignId('user_id')
+                ->constrained()
+                ->onDelete('cascade');
+
+            // Tanggal dan waktu transaksi
+            $table->dateTime('sale_date')->nullable();
+
+            // Total nilai transaksi (jumlah semua items)
+            $table->decimal('total_amount', 15, 2)->default(0);
+
             $table->timestamps();
 
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
-            $table->unsignedBigInteger('product_id');
+            // $table->id();
+            // $table->integer('quantity');
+            // $table->decimal('price', 10, 2);
+            // $table->decimal('total', 10, 2)->nullable();
+            // $table->date('sale_date');
+            // $table->timestamps();
+
+            // $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            // $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+            // $table->unsignedBigInteger('product_id');
         });
     }
 
